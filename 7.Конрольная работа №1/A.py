d29f8154caefef8e48ec95a911ea38b0fb073e7f
GrigorevEv <61190401+GrigorevEv@users.noreply.github.com>
2020-03-16 07:21:45 +0300

    Add files via upload

---
+++ 7.Конрольная работа №1/A.py
@@ -0,0 +1,10 @@
+truck_weight = int(input())
+platform_height = int(input())
+piano_weight = int(input())
+piano_height = int(input())
+fridge_weight = int(input())
+fridge_height = int(input())
+max_weight_on_the_bridge = int(input())
+max_height_on_the_tunnel = int(input())
+
+if max()
