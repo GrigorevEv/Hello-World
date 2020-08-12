db046a780e9131b20e9e0a53c254a1ea2a2352be
GrigorevEv <61190401+GrigorevEv@users.noreply.github.com>
2020-08-12 19:13:50 +0300

    Practice

---
+++ 12.Z-function and KMP algorythm/3. Substring search.py
@@ -0,0 +1,19 @@
+# https://e-maxx.ru/algo/z_function
+
+
+def z_func(s):
+    n = len(s)
+    z = [0] * len(s)
+    l, r = 0, 0  # координаты самого правого отрезка совпадения
+    for i in range(1, n):
+        if i <= r:
+            z[i] = min(r - i + 1, z[i - l])
+        while i + z[i] < n and s[z[i]] == s[i + z[i]]:   # i > r — т.е. текущая позиция лежит за пределами того, что мы уже успели обработать.
+            z[i] += 1
+        if i + z[i] - 1 > r:  # здесь выполняется обновление текущего самого правого отрезка совпадения [l;r]
+            l = i
+            r = i + z[i] - 1
+    return z
+
+
+print(z_func('abaaaba'))
