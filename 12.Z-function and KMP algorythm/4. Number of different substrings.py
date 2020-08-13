c87eaf8929ef53cf7bd12c59ef3734d59b392eb2
GrigorevEv <eug.grigorev@gmail.com>
2020-08-13 17:02:51 +0300

    Practice

--- 12.Z-function and KMP algorythm/4. Number of different substrings.py
+++ 12.Z-function and KMP algorythm/4. Number of different substrings.py
@@ -3,24 +3,16 @@
 # Найти все вхождения второй строки в первую.
 
 
-def z_func(t, p):
-    s = p + '#' + t
+def z_func(s):
     n = len(s)
     k = 0
     z = [0] * len(s)
-    l, r = 0, 0  # координаты самого правого отрезка совпадения
-    for i in range(1, n):
-        if i <= r:
-            z[i] = min(r - i + 1, z[i - l])
-        while i + z[i] < n and s[z[i]] == s[i + z[i]]:   # i > r — т.е. текущая позиция лежит за пределами того, что мы уже успели обработать.
+    for i in range(0, n):
+        while i + z[i] < n and s[z[i]] == s[i + z[i]]:
             z[i] += 1
-        if i + z[i] - 1 > r:  # здесь выполняется обновление текущего самого правого отрезка совпадения [l;r]
-            l = i
-            r = i + z[i] - 1
-    for i in range(n):
-        if z[i] == len(p):
-            k += 1
-    print(k, 'вхожд.')
 
 
-z_func('abaaaba', 'aba')
+
+
+
+z_func('abaaaba')
