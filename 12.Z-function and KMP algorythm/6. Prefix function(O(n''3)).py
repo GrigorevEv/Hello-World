c1e4414dd71ffb1e5e242ffae58add03d4c73e7b
GrigorevEv <61190401+GrigorevEv@users.noreply.github.com>
2020-08-17 07:03:30 +0300

    Practice

---
+++ 12.Z-function and KMP algorythm/6. Prefix function(O(n''3)).py
@@ -0,0 +1,19 @@
+# https://e-maxx.ru/algo/prefix_function
+
+
+def p_func(s):
+    n = len(s)
+    pi = [0] * n
+    for i in range(n):
+        for k in range(1, i + 1):
+            equal = True
+            for j in range(k):
+                if s[j] != s[i - k + 1 + j]:
+                    equal = False
+                    break
+            if equal:
+                pi[i] = k
+    return pi
+
+
+print(p_func('aabaaab'))
