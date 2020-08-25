aa17f34e1e2fde7432de1b1d4d7b321e97b2694e
GrigorevEv <61190401+GrigorevEv@users.noreply.github.com>
2020-08-25 21:44:50 +0300

    Practice

--- 13.Arithmetic expressions and calculators/Check braces sequence.py
+++ 13.Arithmetic expressions and calculators/Check braces sequence.py
@@ -3,31 +3,51 @@ import Stack_LIFO
 def is_braces_sequence_correct(s: str):
     """
     Проверяет корректность скобочной последовательности
-    из круглых и квадратных скобок ()[]
-    >>> is_braces_sequence_correct('(([()]))[]')
+    из круглых, квадратных, и наклонных скобок ()[]//
+
+    >>> is_braces_sequence_correct('///')
+    False
+    >>> is_braces_sequence_correct('45')
+    True
+    >>> is_braces_sequence_correct('//[]')
+    True
+    >>> is_braces_sequence_correct('/(([()]))/[]//')
+    True
+    >>> is_braces_sequence_correct('/([])/[]')
     True
     >>> is_braces_sequence_correct('(](]')
     False
     >>> is_braces_sequence_correct('(')
     False
+    >>> is_braces_sequence_correct('([[[]]])')
+    True
     >>> is_braces_sequence_correct(']')
     False
+    >>> is_braces_sequence_correct('/')
+    False
+    >>> is_braces_sequence_correct('(/)/')
+    False
     """
+    flag = True
+    Stack_LIFO.clear()
     for brace in s:
-        if brace not in '()[]':
+        if brace not in '()[]/':
             continue
-        if brace in '([':
+        if brace in '([' or flag == True and brace in '/':
             Stack_LIFO.push(brace)
+            if brace in '/':
+                flag = False
         else:
-            assert brace in ')]', 'Ожидалась закрывающая скобка: ' + str(brace)
             if Stack_LIFO.is_empty():
                 return False
             left = Stack_LIFO.pop()
-            assert left in '([', 'Ожидалась открывающая скобка: ' + str(brace)
             if left == '(':
                 right = ')'
             elif left == '[':
                 right = ']'
+            elif left == '/':
+                right = '/'
+                flag = True
             if right != brace:
                 return False
     return Stack_LIFO.is_empty()
