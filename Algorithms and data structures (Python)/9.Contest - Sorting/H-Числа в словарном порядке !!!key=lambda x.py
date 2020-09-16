905d74736c89e19b09855eed6ed046627821e88a
GrigorevEv <eug.grigorev@gmail.com>
2020-09-16 20:49:36 +0300

    Practise

--- Algorithms and data structures (Python)/9.Contest - Sorting/H-Числа в словарном порядке !!!key=lambda x.py
+++ Algorithms and data structures (Python)/9.Contest - Sorting/H-Числа в словарном порядке !!!key=lambda x.py
@@ -6,14 +6,11 @@
 # Числа по возрастанию единиц, при равных единиц - по возрастанию десятков, при равных единицах и десятков - по возрастанию сотен и тп.
 
 
-
-
 N = int(input())
-s = list(map(int,input().split()))
+s = list(map(int, input().split()))
 s = sorted(s, key=lambda x: (x % 10, x % 100, x % 1000))
 for i in range(len(s)):
     print(s[i], end=' ')
-
 
 
 # https://webdevblog.ru/kak-ispolzovat-v-python-lyambda-funkcii/
