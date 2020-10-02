952d4160f4f00e4973ed2c48b2b72b19a381d268
GrigorevEv <eug.grigorev@gmail.com>
2020-10-02 17:25:03 +0300

    Practice

--- Algorithms and data structures (Python)/O. AUTUMN OFFICE (TICKETS)/J. One-pass algorithms for processing a sequence(count, sum, product).py
+++ Algorithms and data structures (Python)/O. AUTUMN OFFICE (TICKETS)/J. One-pass algorithms for processing a sequence(count, sum, product).py
@@ -1,12 +1,36 @@
 # Однопроходные алгоритмы обработки последовательности: подсчёт, сумма, произведение
+"""
+>>> count_all_elements([1, 2, 3, 4, 5])
+5
+>>> sum_all_elements([1, 2, 3, 4, 5])
+15
+>>> mult_all_elements([1, 2, 3, 4, 5])
+120
+"""
+
 
 def count_all_elements(x: list):
-    pass
+    k = 0
+    for i in range(len(x)):
+        k += 1
+    return k
 
 
 def sum_all_elements(x: list):
-    pass
+    k = 0
+    for i in range(len(x)):
+        k += x[i]
+    return k
 
 
 def mult_all_elements(x: list):
-    pass
+    k = 1
+    for i in range(len(x)):
+        k *= x[i]
+    return k
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(verbose=True)
+
