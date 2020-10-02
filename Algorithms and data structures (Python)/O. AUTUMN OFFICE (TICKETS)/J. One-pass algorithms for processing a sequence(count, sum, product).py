65ef85786d3b40071b09e343783e8adf389877d8
GrigorevEv <eug.grigorev@gmail.com>
2020-10-02 11:42:42 +0300

    Practice

---
+++ Algorithms and data structures (Python)/O. AUTUMN OFFICE (TICKETS)/J. One-pass algorithms for processing a sequence(count, sum, product).py
@@ -0,0 +1,12 @@
+# Однопроходные алгоритмы обработки последовательности: подсчёт, сумма, произведение
+
+def count_all_elements(x: list):
+    pass
+
+
+def sum_all_elements(x: list):
+    pass
+
+
+def mult_all_elements(x: list):
+    pass
