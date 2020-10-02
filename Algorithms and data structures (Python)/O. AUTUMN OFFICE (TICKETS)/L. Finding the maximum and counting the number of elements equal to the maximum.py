3781211c5552e2e8df8c025e6da75dfb85822815
GrigorevEv <61190401+GrigorevEv@users.noreply.github.com>
2020-10-02 20:17:14 +0300

    Practice

---
+++ Algorithms and data structures (Python)/O. AUTUMN OFFICE (TICKETS)/L. Finding the maximum and counting the number of elements equal to the maximum.py
@@ -0,0 +1,17 @@
+# Нахождение трёх максимальных элементов в последовательности за один проход.
+"""
+>>> three_max_elements([1, 2, 5, 6, 3, 4, 7, 8, 9])
+7 8 9
+>>> three_max_elements([1, 2, 7, 6, 3, 4, 7, 0, 9])
+7 7 9
+"""
+
+
+
+def three_max_elements(x: list):
+    a = b = c = 0
+    for i in range(len(x)):
+        if x[i] >= a:
+            a, b, c = x[i], a, b
+    print(c, b, a, end='')
+
