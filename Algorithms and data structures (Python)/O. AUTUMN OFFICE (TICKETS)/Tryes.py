6c632bac3d482ecf66257d81c4266008a0074911
GrigorevEv <61190401+GrigorevEv@users.noreply.github.com>
2020-10-07 20:20:19 +0300

    Practice

--- Algorithms and data structures (Python)/O. AUTUMN OFFICE (TICKETS)/Tryes.py
+++ Algorithms and data structures (Python)/O. AUTUMN OFFICE (TICKETS)/Tryes.py
@@ -1,4 +1,9 @@
-a = True
-b = False
-c = a + b
-print(c)
+l, m, r = [1, 4], [2], [3]
+a = l + m + r
+print(a)
+k = 0
+b = [0]*10
+for i in l + m + r:
+    b[k] = i
+    k += 1
+print(b)
--- Algorithms and data structures (Python)/O. AUTUMN OFFICE (TICKETS)/X. Quicksort (Tony Hoare).py
+++ Algorithms and data structures (Python)/O. AUTUMN OFFICE (TICKETS)/X. Quicksort (Tony Hoare).py
@@ -1,6 +1,6 @@
 # Сортировка Тони Хоара или быстрая сортировка
 
-def q_sort(a):
+def q_sort(a: list):
     if len(a) <= 1:
         return
     barrier = a[0]
@@ -8,17 +8,18 @@ def q_sort(a):
     for x in a:
         if x < barrier:
             l.append(x)
+
         elif x == barrier:
             m.append(x)
         else:
             r.append(x)
-        k = 0
-        q_sort(l)
-        q_sort(r)
-        for i in l + m + r:
-            a[k] = i
-            k += 1
-        return a
+    q_sort(l)
+    q_sort(r)
+    k = 0
+    for i in l + m + r:
+        a[k] = i
+        k += 1
+    return a
 
 
-print(q_sort([5, 4, 3, 2, 1, 0]))
+print(q_sort([3, 2, 1, 5, 4, 0]))
