ddae925d7763e8f4cec72cd4810d2a8396bf6f94
GrigorevEv <61190401+GrigorevEv@users.noreply.github.com>
2020-10-09 17:57:04 +0300

    Practice

---
+++ Algorithms and data structures (Python)/O. AUTUMN OFFICE (TICKETS)/ZE. Hanoi towers.py
@@ -0,0 +1,26 @@
+# Ханойские башни
+# https://www.geeksforgeeks.org/python-program-for-tower-of-hanoi/
+# Tower of Hanoi is a mathematical puzzle where we have three rods and n disks.
+# The objective of the puzzle is to move the entire stack to another rod, obeying the following simple rules:
+# 1) Only one disk can be moved at a time.
+# 2) Each move consists of taking the upper disk from one of the stacks and placing it on top
+# of another stack i.e. a disk can only be moved if it is the uppermost disk on a stack.
+# 3) No disk may be placed on top of a smaller disk.
+# Note: Transferring the top n-1 disks from source rod to Auxiliary rod can again be thought of as a
+# fresh problem and can be solved in the same manner.
+# Recursive Python function to solve the tower of hanoi
+
+
+def tower_of_hanoi(n, source, destination, auxiliary):
+    if n == 1:
+        print("Move disk 1 from source", source, "to destination", destination)
+        return
+    tower_of_hanoi(n - 1, source, auxiliary, destination)
+    print("Move disk", n, "from source", source, "to destination", destination)
+    tower_of_hanoi(n - 1, auxiliary, destination, source)
+
+
+# Driver code
+n = 3
+tower_of_hanoi(n, 'A', 'B', 'C')
+# A, C, B are the name of rods
