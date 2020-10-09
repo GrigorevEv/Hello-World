d03882e01baa426490f01f2e12ac60c62be13de3
GrigorevEv <61190401+GrigorevEv@users.noreply.github.com>
2020-10-09 17:20:09 +0300

    Practice

---
+++ Algorithms and data structures (Python)/O. AUTUMN OFFICE (TICKETS)/ZC. Fast exponentiation.py
@@ -0,0 +1,14 @@
+# Быстрое возведение в степень.
+
+
+def pow(a: float, n: int):
+    if n == 0:
+        return 1
+    elif n % 2 == 1:
+        return pow(a, n-1) * a
+    else:
+        return pow(a**2, n//2)
+
+
+print(pow(3, 20))
+
