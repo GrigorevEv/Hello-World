562e75f340e285c3491ac77c7be48ee52d2629c6
GrigorevEv <eug.grigorev@gmail.com>
2020-10-10 17:01:44 +0300

    Practise

--- Algorithms and data structures (Python)/O. AUTUMN OFFICE (TICKETS)/ZB. Euclid's Algorithm. Implementation through a loop and through recursion.py
+++ Algorithms and data structures (Python)/O. AUTUMN OFFICE (TICKETS)/ZB. Euclid's Algorithm. Implementation through a loop and through recursion.py
@@ -1,22 +1,12 @@
 # Алгоритм Евклида. Реализация через цикл и через рекурсию
 
-def gcd_recursion(a=int(input()), b=int(input())):
+def gcd(a, b):
     if b == 0:
         return a
     else:
-        return gcd_recursion(b, a % b)
+        return gcd(b, a % b)
 
 
-print(gcd_recursion())
-
-
-def gcd_cycle(a=int(input()), b=int(input())):
-    while a != 0 and b != 0:
-        if a > b:
-            a = a % b
-        else:
-            b = b % a
-    return a+b
-
-
-print(gcd_cycle())
+c = int(input())
+d = int(input())
+print(gcd(c, d))
