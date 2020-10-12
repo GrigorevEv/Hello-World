2aca090e6cbfa3391873ca076b960dbdc964cd4b
GrigorevEv <61190401+GrigorevEv@users.noreply.github.com>
2020-10-12 21:26:45 +0300

    Practice

--- Algorithms and data structures (Python)/Q.Dictionaries and Sets in Python and Asymptotics of Standard Operations/B. Task - 1.py
+++ Algorithms and data structures (Python)/Q.Dictionaries and Sets in Python and Asymptotics of Standard Operations/B. Task - 1.py
@@ -17,15 +17,15 @@ def get_pop_time(size, repeat_count, pop_position=None):
 
 
 repeat_count = 1000
-# code here
-# values1 = [get_pop_time(...) for size in range(10, 1000)]
-# values2 = [get_pop_time(...) for size in range(10, 1000)]
-# values3 = [get_pop_time(...) for size in range(10, 1000)]
-#
-# plt.plot(values1, label='Pop no args')
-# plt.plot(values2, label='Pop start list')
-# plt.plot(values3, label='Pop end list')
-# plt.ylabel('pop time')
-# ax = plt.subplot(111)
-# ax.legend()
-# plt.show()
+
+values1 = [get_pop_time(10000, 1000, 50) for size in range(10, 1000)]
+values2 = [get_pop_time(10000, 2000, 100) for size in range(10, 1000)]
+values3 = [get_pop_time(10000, 3000, 150) for size in range(10, 1000)]
+
+plt.plot(values1, label='Pop no args')
+plt.plot(values2, label='Pop start list')
+plt.plot(values3, label='Pop end list')
+plt.ylabel('pop time')
+ax = plt.subplot(111)
+ax.legend()
+plt.show()
