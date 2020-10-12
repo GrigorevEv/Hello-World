7090d96877f4adf0a30fbddb2b17bd61a3b4775d
GrigorevEv <eug.grigorev@gmail.com>
2020-10-12 08:24:12 +0300

    Practice

--- Algorithms and data structures (Python)/O. AUTUMN OFFICE (TICKETS)/ZE. Hanoi towers.py
+++ Algorithms and data structures (Python)/O. AUTUMN OFFICE (TICKETS)/ZE. Hanoi towers.py
@@ -11,13 +11,13 @@
 # Recursive Python function to solve the tower of hanoi
 
 
-def tower_of_hanoi(n, source, auxiliary, destination):
+def tower_of_hanoi(n, source, destination, auxiliary):
     if n == 1:
         print("Move disk 1 from source", source, "to destination", destination)
         return
-    tower_of_hanoi(n - 1, source, destination, auxiliary)
+    tower_of_hanoi(n - 1, source, auxiliary, destination)
     print("Move disk", n, "from source", source, "to destination", destination)
-    tower_of_hanoi(n - 1, auxiliary, source, destination)
+    tower_of_hanoi(n - 1, auxiliary, destination, source)
 
 
 # Driver code
