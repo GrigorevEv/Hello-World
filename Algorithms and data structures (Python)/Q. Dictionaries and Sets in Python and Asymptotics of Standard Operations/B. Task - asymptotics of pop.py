33884098c92c9ebc190c495623a31bc4e4a4eb37
GrigorevEv <eug.grigorev@gmail.com>
2020-10-13 11:43:42 +0300

    Practise

--- Algorithms and data structures (Python)/Q. Dictionaries and Sets in Python and Asymptotics of Standard Operations/B. Task - asymptotics of pop.py
+++ Algorithms and data structures (Python)/Q. Dictionaries and Sets in Python and Asymptotics of Standard Operations/B. Task - asymptotics of pop.py
@@ -11,16 +11,22 @@ def get_pop_time(size, repeat_count, pop_position=None):
     l = [0] * size
     start_time = time.time()
     for i in range(repeat_count):
-        l.pop(pop_position)
+        while len(l) > 0:
+            if pop_position == None:
+                l.pop()
+            elif pop_position == size:
+                l.pop(len(l) - 1)
+            else:
+                l.pop(pop_position)
     end_time = time.time()
     return (end_time - start_time) / repeat_count
 
 
 repeat_count = 1000
-
-values1 = [get_pop_time(10000, 1000, 50) for size in range(10, 1000)]
-values2 = [get_pop_time(10000, 2000, 100) for size in range(10, 1000)]
-values3 = [get_pop_time(10000, 3000, 150) for size in range(10, 1000)]
+size = 1000
+values1 = [get_pop_time(size, repeat_count) for i in range(10, 1000)]
+values2 = [get_pop_time(size, repeat_count, 0) for j in range(10, 1000)]
+values3 = [get_pop_time(size, repeat_count, size) for k in range(10, 1000)]
 
 plt.plot(values1, label='Pop no args')
 plt.plot(values2, label='Pop start list')
---
+++ Algorithms and data structures (Python)/Q. Dictionaries and Sets in Python and Asymptotics of Standard Operations/tr.py
@@ -0,0 +1,5 @@
+a = [0] * 10
+k = 1
+while len(a) > 0:
+    a.pop(len(a) - 1)
+    print(len(a))
