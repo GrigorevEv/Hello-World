6115eec87d3b91a7a9454083a7717313d9e48db0
GrigorevEv <61190401+GrigorevEv@users.noreply.github.com>
2020-10-14 11:38:25 +0300

    Practice

--- Algorithms and data structures (Python)/Q. Dictionaries and Sets in Python and Asymptotics of Standard Operations/G. Task 1 (dict) Counting words !!!string,operator,open,write!!!.py
+++ Algorithms and data structures (Python)/Q. Dictionaries and Sets in Python and Asymptotics of Standard Operations/G. Task 1 (dict) Counting words !!!string,operator,open,write!!!.py
@@ -1,24 +1,19 @@
 import string
 import operator
 
-file = open(r'C:\Users\Eugene\Desktop\LICENSES.txt')
-formated_txt = open(r'C:\Users\Eugene\Desktop\dict.txt', 'w')
+file = open('text1.txt')
+formated_txt = open('text2.txt', 'w')
 file = file.read()
 punctuation = string.punctuation
-
 for index in file:
     if index not in punctuation and index not in '\n':
         formated_txt.write(index.lower())
-
 formated_txt.close()
 
 dictionary = dict()
-formated_txt = open(r'C:\Users\Eugene\Desktop\dict.txt')
+formated_txt = open('text2.txt')
 formated_txt = formated_txt.read()
-
-
 array = list(map(str, formated_txt.split()))
-
 for key in array:
     if key in dictionary:
         dictionary[key] += 1
@@ -26,12 +21,9 @@ for key in array:
         dictionary[key] = 1
 
 dictionary = sorted(dictionary.items(), key=operator.itemgetter(1), reverse=True)
-formated_txt = open(r'C:\Users\Eugene\Desktop\dict.txt', 'w')
-
+formated_txt = open('text2.txt', 'w')
 for a in dictionary:
     print(a[0], ':', a[1])
     formated_txt.write(a[0] + ' : ' + str(a[1]) + '\n')
 
 formated_txt.close()
-
-
