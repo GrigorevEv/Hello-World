972c280fc35e114057b2ef1c1c68775ea5f2acf5
GrigorevEv <61190401+GrigorevEv@users.noreply.github.com>
2020-10-20 16:47:53 +0300

    Practice

--- Algorithms and data structures (Python)/Q. Dictionaries and Sets in Python and Asymptotics of Standard Operations/tr.py
+++ Algorithms and data structures (Python)/Q. Dictionaries and Sets in Python and Asymptotics of Standard Operations/tr.py
@@ -1,20 +1,93 @@
-# coding=utf-8
-import copy
+# Import a library of functions called 'pygame'
+import pygame
+from math import pi
 
-en_ru = {'home': 'домашняя папка', 'mouse': 'манипулятор мышь'}
-ru_en = {'дом': 'home', 'мышь': 'mouse'}
-en_ru_act = en_ru.copy()
-ru_en_act = ru_en.copy()
+# Initialize the game engine
+pygame.init()
 
-for key_en_ru_act in en_ru:
-    for key_ru_en in ru_en:
-        if key_en_ru_act == ru_en[key_ru_en]:
-            en_ru_act[key_en_ru_act] = en_ru_act[key_en_ru_act] + ', ' + key_ru_en
+# Define the colors we will use in RGB format
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
 
-for key_en_ru in en_ru:
-    if en_ru[key_en_ru] not in ru_en:
-        ru_en_act[en_ru[key_en_ru]] = key_en_ru
-print(en_ru)
-print(ru_en)
-print(en_ru_act)
-print(ru_en_act)
+# Set the height and width of the screen
+size = [400, 300]
+screen = pygame.display.set_mode(size)
+
+pygame.display.set_caption("Example code for the draw module")
+
+# Loop until the user clicks the close button.
+done = False
+clock = pygame.time.Clock()
+
+while not done:
+
+    # This limits the while loop to a max of 10 times per second.
+    # Leave this out and we will use all CPU we can.
+    clock.tick(10)
+
+    for event in pygame.event.get():  # User did something
+        if event.type == pygame.QUIT:  # If user clicked close
+            done = True  # Flag that we are done so we exit this loop
+
+    # All drawing code happens after the for loop and but
+    # inside the main while done==False loop.
+
+    # Clear the screen and set the screen background
+    screen.fill(WHITE)
+
+    # Draw on the screen a GREEN line from (0, 0) to (50, 30) 
+    # 5 pixels wide.
+    pygame.draw.line(screen, GREEN, [0, 0], [50, 30], 5)
+
+    # Draw on the screen 3 BLACK lines, each 5 pixels wide.
+    # The 'False' means the first and last points are not connected.
+    pygame.draw.lines(screen, BLACK, False, [[0, 80], [50, 90], [200, 80], [220, 30]], 5)
+
+    # Draw on the screen a GREEN line from (0, 50) to (50, 80) 
+    # Because it is an antialiased line, it is 1 pixel wide.
+    pygame.draw.aaline(screen, GREEN, [0, 50], [50, 80], True)
+
+    # Draw a rectangle outline
+    pygame.draw.rect(screen, BLACK, [75, 10, 50, 20], 2)
+
+    # Draw a solid rectangle
+    pygame.draw.rect(screen, BLACK, [150, 10, 50, 20])
+
+    # Draw a rectangle with rounded corners
+    pygame.draw.rect(screen, GREEN, [115, 210, 70, 40], 10)
+    pygame.draw.rect(screen, RED, [135, 260, 50, 30], 0)
+
+    # Draw an ellipse outline, using a rectangle as the outside boundaries
+    pygame.draw.ellipse(screen, RED, [225, 10, 50, 20], 2)
+
+    # Draw an solid ellipse, using a rectangle as the outside boundaries
+    pygame.draw.ellipse(screen, RED, [300, 10, 50, 20])
+
+    # This draws a triangle using the polygon command
+    pygame.draw.polygon(screen, BLACK, [[100, 100], [0, 200], [200, 200]], 5)
+
+    # Draw an arc as part of an ellipse. 
+    # Use radians to determine what angle to draw.
+    pygame.draw.arc(screen, BLACK, [210, 75, 150, 125], 0, pi / 2, 2)
+    pygame.draw.arc(screen, GREEN, [210, 75, 150, 125], pi / 2, pi, 2)
+    pygame.draw.arc(screen, BLUE, [210, 75, 150, 125], pi, 3 * pi / 2, 2)
+    pygame.draw.arc(screen, RED, [210, 75, 150, 125], 3 * pi / 2, 2 * pi, 2)
+
+    # Draw a circle
+    pygame.draw.circle(screen, BLUE, [60, 250], 40)
+
+    # Draw only one circle quadrant
+    pygame.draw.circle(screen, BLUE, [250, 250], 40, 0)
+    pygame.draw.circle(screen, RED, [250, 250], 40, 30)
+    pygame.draw.circle(screen, GREEN, [250, 250], 40, 20)
+    pygame.draw.circle(screen, BLACK, [250, 250], 40, 10)
+
+    # Go ahead and update the screen with what we've drawn.
+    # This MUST happen after all the other drawing commands.
+    pygame.display.flip()
+
+# Be IDLE friendly
+pygame.quit()
