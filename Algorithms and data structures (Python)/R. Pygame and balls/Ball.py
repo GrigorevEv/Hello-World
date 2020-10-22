ab0f465bca20084d7173c1f7a6ab174830ef5b1e
GrigorevEv <61190401+GrigorevEv@users.noreply.github.com>
2020-10-22 22:09:00 +0300

    Practice

--- Algorithms and data structures (Python)/R. Pygame and balls/Ball.py
+++ Algorithms and data structures (Python)/R. Pygame and balls/Ball.py
@@ -18,13 +18,19 @@ vy = 30
 while True:
     dt = clock.tick(50)/300  # The program will never run at more than 50 frames per second
 
-    for event in pygame.event.get():  # Get events from the queue
-        if event.type == pygame.QUIT or event.type == pygame.KEYDOWN:  # If user press X or KEYDOWN - program stoped
-            sys.exit()
-    if pygame.key.get_pressed()[pygame.K_RIGHT]:
+    # for event in pygame.event.get():  # Get events from the queue
+    #     if event.type == pygame.QUIT or event.type == pygame.KEYDOWN:  # If user press X or KEYDOWN - program stoped
+    #         sys.exit()
+
+    key = pygame.key.get_pressed()
+    if key[pygame.K_RIGHT]:
         vx = abs(vx)
-    elif pygame.key.get_pressed()[pygame.K_LEFT]:
+    elif key[pygame.K_LEFT]:
         vx = -abs(vx)
+    elif key[pygame.K_DOWN]:
+        vy = abs(vy)
+    elif key[pygame.K_UP]:
+        vy = -abs(vy)
 
     x += vx * dt
     y += vy * dt
@@ -37,3 +43,4 @@ while True:
     pygame.draw.circle(screen, (200, 100, 50), (int(x), int(y)), 20, 6)
 
     pygame.display.flip()  # refreshes the screen and displays everything
+    pygame.event.pump()
