59bd69400618bb3e7c32707d35cb2d1145e3e463
GrigorevEv <61190401+GrigorevEv@users.noreply.github.com>
2020-11-16 21:34:25 +0300

    Practice

--- Algorithms and data structures (Python)/R. Pygame and balls/Vector.py
+++ Algorithms and data structures (Python)/R. Pygame and balls/Vector.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+
 class Vector:
     def __init__(self, x=0, y=0):
         self.x = x
@@ -22,15 +24,25 @@ class Vector:
         return 'Vector(' + str(self.x) + ', ' + str(self.y) + ')'
 
 
-a = Vector(1, 2)
-b = Vector(3, 4)
-c = a + b
-d = a - b
-e = a * b
-f = b * a
+# a = Vector(1, 2)
+# b = Vector(3, 4)
+# c = a + b
+# d = a - b
+# e = a * b
+# f = b * a
+#
+# print(c.__str__())
+# print(d.__str__())
+# print(e.__str__())
+# print(f.__str__())
+# print(d.__neg__())
 
-print(c.__str__())
-print(d.__str__())
-print(e.__str__())
-print(f.__str__())
-print(d.__neg__())
+# Используя класс Vector выведите координаты центра масс данного множества точек.
+
+dot_count = int(input())  # количество точек
+dot_array = []  # массив точек
+for i in range(dot_count):
+    dot = Vector(int(input()), int(input()))
+    print(dot.__str__())
+    dot_array.append(dot)
+print(dot_array)
