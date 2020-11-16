e6069c55217f5d9dba11a82ec32a963d0b2539c9
GrigorevEv <61190401+GrigorevEv@users.noreply.github.com>
2020-11-16 21:15:26 +0300

    Practice

--- Algorithms and data structures (Python)/R. Pygame and balls/Vector.py
+++ Algorithms and data structures (Python)/R. Pygame and balls/Vector.py
@@ -6,10 +6,31 @@ class Vector:
     def __add__(self, other):
         return Vector(self.x + other.x, self.y + other.y)
 
+    def __sub__(self, other):
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other):
+        return Vector(self.x * other.x, self.y * other.y)
+
+    def __rmul__(self, other):
+        return Vector(self.x * other.x, self.y * other.y)
+
+    def __neg__(self):
+        return Vector(-self.x, -self.y)
+
     def __str__(self):
         return 'Vector(' + str(self.x) + ', ' + str(self.y) + ')'
+
 
 a = Vector(1, 2)
 b = Vector(3, 4)
 c = a + b
+d = a - b
+e = a * b
+f = b * a
+
 print(c.__str__())
+print(d.__str__())
+print(e.__str__())
+print(f.__str__())
+print(d.__neg__())
